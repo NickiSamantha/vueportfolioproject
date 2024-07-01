1f9473c90edaad435b6721a7ca204f5562905336
NickiSamantha <nickisabels73@gmail.com>
2024-07-01 09:40:39 +0200

    navbar complete

---
+++ src/components/NavBar.vue
@@ -0,0 +1,56 @@
+<template>
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  
+    <div class="container-fluid">
+  
+      <a class="navbar-brand" href="#landing">
+          <img src="https://nickisamantha.github.io/allImages/images/logo.png" alt="logo" class="img-fluid" loading="lazy">
+      </a>
+  
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+  
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+  
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+  
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#landing">Home</a>
+          </li>
+  
+          <li class="nav-item">
+            <a class="nav-link" href="#about">About</a>
+          </li>
+  
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#resume" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Resume
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#education">Education</a></li>
+              <li><a class="dropdown-item" href="#skills">Skills</a></li>
+              <li><a class="dropdown-item" href="#workexperience">Work Experience</a></li>
+            </ul>
+          </li>
+  
+          <li class="nav-item">
+            <a class="nav-link" href="#testimonials">Testimonials</a>
+          </li>
+  
+          <li class="nav-item">
+            <a class="nav-link" href="#project">Project</a>
+          </li>
+  
+          <li class="nav-item">
+            <a class="nav-link" href="#reachMe">Reach Me</a>
+          </li>
+  
+        </ul>
+      </div>
+    </div>
+  </nav>
+  </template>
+
+  <!-- No Script or Style needed for NavBar -->
